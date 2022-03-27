a5d02f3264439411752d02ab51efe31b2b99ffad
soltrawberry <80199797+soltrawberry@users.noreply.github.com>
2022-03-27 19:10:43 +0900

    image processing and practice week03

---
+++ ch03/01.variable.py
@@ -0,0 +1,13 @@
+variable1 = 100
+variable2 = 3.14
+variable3 = -200
+variable4 = 1.2 + 3.4j
+variable5 = 'Ths is Python'
+
+variable6 = True
+variable7 = float(variable1)
+variable8 = int(variable2)
+
+print('variable1 =', variable1, type(variable1))
+print('variable2 =' , variable2, type(variable2))
+print('variable3 = ', variable3, type(variable3))
